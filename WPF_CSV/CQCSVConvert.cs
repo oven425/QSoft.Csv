424155d67618acfb45aac04844a6e5888a043cdb
QQ <oven425@yahoo.com.tw>
2018-12-28 18:09:16 +0800

    Add files via upload

---
+++ WPF_CSV/CQCSVConvert.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace QCSV
+{
+    public class CQCSVConvert
+    {
+        public static T DeserializeObject<T>(string data) where T:new()
+        {
+            T aa = new T();
+            string[] sl_1 = data.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+            return aa;
+        }
+
+        public static string SerializeObject(object data, string split = ",")
+        {
+            StringBuilder strb = new StringBuilder();
+
+            Type type = data.GetType();
+            MethodInfo method_count = type.GetMethod("get_Count");
+            int count = (int)method_count.Invoke(data, null);
+
+            MethodInfo method = type.GetMethod("get_Item");
+            for (int i = 0; i < count; i++)
+            {
+                
+                object vv = method.Invoke(data, new object[] { i });
+                Type type1 = vv.GetType();
+                PropertyInfo[] pps = type1.GetProperties();
+                StringBuilder strb1 = null;
+                if (i == 0)
+                {
+                    strb1 = new StringBuilder();
+                }
+                StringBuilder strb2 = new StringBuilder();
+                
+                foreach (PropertyInfo pi in pps)
+                {
+                    if(i==0)
+                    {
+                        object[] ccu = pi.GetCustomAttributes(typeof(CQCSVProperty), true);
+                        string col_name = pi.Name;
+                        if(ccu.Length >0)
+                        {
+                            if(string.IsNullOrEmpty(((CQCSVProperty)ccu[0]).Name) == false)
+                            {
+                                col_name = ((CQCSVProperty)ccu[0]).Name;
+                            }
+                        }
+                        if (strb1.Length > 0)
+                        {
+                            strb1.Append(",");
+                            strb1.Append(col_name);
+                        }
+                        else
+                        {
+                            strb1.Append(col_name);
+                        }
+                    }
+                    
+
+                    object oob = pi.GetValue(vv, null);
+                    if(strb2.Length >0)
+                    {
+                        strb2.Append(",");
+                        strb2.Append(oob.ToString());
+                    }
+                    else
+                    {
+                        strb2.Append(oob.ToString());
+                    }
+                    
+                }
+                if (strb1 != null)
+                {
+                    strb.AppendLine(strb1.ToString());
+                }
+                strb.AppendLine(strb2.ToString());
+            }
+
+            return strb.ToString();
+        }
+    }
+
+    public class CQCSVProperty : Attribute
+    {
+        public string Name { set; get; }
+        public string Format { set; get; }
+    }
+}
---
+++ WPF_CSV/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using QCSV;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CSV
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();         
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //List<CQData> ss = new List<CQData>();
+            //ss.Add(new CQData());
+            //ss.Add(new CQData());
+            //ss.Add(new CQData());
+            //string str = CQCSVConvert.SerializeObject(ss);
+            //File.WriteAllText("AAA.txt", str);
+            string data = File.ReadAllText("AAA.txt");
+            CQCSVConvert.DeserializeObject<List<CQData>>(data);
+        }
+    }
+
+    
+
+    public class CQData
+    {
+        public CQData()
+        {
+            this.ID = DateTime.Now.Millisecond;
+            this.Name = "AA";
+            this.Time = DateTime.Now;
+        }
+        //[CQCSVProperty(Name="編號")]
+        public int ID { set; get; }
+        //[CQCSVProperty(Name = "名稱")]
+        public string Name { set; get; }
+        public DateTime Time { set; get; }
+    }
+
+}
