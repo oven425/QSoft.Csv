8032e6bb021a644671e8e0b860f03f24446b2713
oven425 <oven425@yahoo.com.tw>
2024-08-20 22:07:22 +0800

    update
    1. test source generate

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Lib;
+
+public class Base
+{
+}
+
+public class Foo1 : Base
+{
+}
+
+public class Foo2 : IFoo
+{
+}
+
+public class Foo3 : Base, IFoo
+{
+}
+
+public class Foo4 : IFoo
+{
+}
+
+public interface IFoo
+{
+}
